b0f1c341d8990674f3ae1fdfd37fa778d032d951
Suyash Parganiha <88725447+Suyash30030@users.noreply.github.com>
2025-08-12 01:07:21 +0530

    Update Techstack.jsx

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -1,45 +1,40 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
   DiGit,
-  DiJava,
   DiPostgresql,
+  DiPython,
 } from "react-icons/di";
 import {
-  SiHtml5,
-  SiExpress,
-  SiCss3
+  SiCss3,
+  SiPowerbi,
+  SiMicrosoftexcel,
+  SiGooglesheets,
 } from "react-icons/si";
 
 const Techstack = () => {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <SiPowerbi />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPostgresql />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
+        <SiMicrosoftexcel />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCss3 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
+        <SiGooglesheets />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
